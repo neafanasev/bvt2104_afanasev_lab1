c9fb38c34c27ac6854de584d674a9ea428879ccb
neafanasev <neafanasev@gmail.com>
2022-10-04 13:44:56 +0300

    edited functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,9 @@
-# slow version of the function
+# optimal version of the function
 
 def is_prime(n):
-    c = 0
-    for i in range(1, n+1):
-        if n % i == 0:
-            c += 1
-    if c > 2:
-        return False
-    else:
-        return True
+    c = 2
+    while c < int(n**0.5 + 1):
+        if n % c == 0:
+            return True
+    return False
 
