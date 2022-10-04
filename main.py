3ccfbd369f1b88c8bb0181f7a552c17f5682b0e9
neafanasev <neafanasev@gmail.com>
2022-10-05 00:53:19 +0300

    added is_odd func

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-# this program shows how many given numbers is prime
-
 from functions import is_prime, is_even
-
+from func1 import *
 
 n = int(input('Enter count of numbers: '))
-c = 0
-e = 0
+c, e, s = 0, 0, 0
 
 print('Enter numbers: ')
 for i in range(n):
@@ -14,10 +11,12 @@ for i in range(n):
         c += 1
     if is_even(a):
         e += 1
-
+    if is_odd(a):
+        s += 1
 
 print('Prime numbers total: ' + str(c))
 print('Even numbers total: ' + str(e))
+print('Special numbers total: ' + str(s))
 
 
 
